c3079a13b4b82a50c18206374f3464b720c437e8
javaheri <asdasdasdasd@example.com>
2024-10-09 13:24:02 +0330

    Mongo

--- PublicFunction/DataBase/Mongo/Mongo.cs
+++ PublicFunction/DataBase/Mongo/Mongo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 
@@ -9,35 +10,32 @@ namespace PublicFunction.DataBase.Mongo
 {
     public class TBaseDocumentModel
     {
+        public ObjectId Id { get; set; } // Default field for MongoDB document ID
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
+
     interface IMongo
     {
-        public bool Insert<T>(string CollectionName, T BaseCollection) where T : TBaseDocumentModel;
-        public bool Insert(string CollectionName, List<Dictionary<string, object>> BaseCollection);
-        public bool Insert(string CollectionName, Dictionary<string, object> BaseCollection);
+        bool Insert<T>(string CollectionName, T BaseCollection) where T : TBaseDocumentModel;
+        bool Insert(string CollectionName, List<Dictionary<string, object>> BaseCollection);
+        bool Insert(string CollectionName, Dictionary<string, object> BaseCollection);
+        bool Update<T>(string CollectionName, FilterDefinition<T> filter, UpdateDefinition<T> update) where T : TBaseDocumentModel;
+        bool Delete<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel;
+        List<T> Query<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel;
+        T QueryOne<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel;
     }
-    public class Mongo : IMongo {
-        
+
+    public class Mongo : IMongo
+    {
         private readonly IConfiguration Configuration;
 
-        private string ConnectionString
-        {
-            get
-            {
-                return Configuration["PublicFunction:DataBase:Mongo:ConnectionString"].ToString();
-            }
-        }
-        private string DatabaseName
-        {
-            get
-            {
-                return Configuration["PublicFunction:DataBase:Mongo:DatabaseName"].ToString();
-            }
-        }
+        private string ConnectionString => Configuration["PublicFunction:DataBase:Mongo:ConnectionString"];
+
+        private string DatabaseName => Configuration["PublicFunction:DataBase:Mongo:DatabaseName"];
 
         private static MongoClient _MongoClient;
-
         private static IMongoDatabase _IMongoDatabase;
+
         public Mongo(IConfiguration configuration)
         {
             try
@@ -48,7 +46,7 @@ namespace PublicFunction.DataBase.Mongo
             }
             catch (Exception ex)
             {
-                throw ex;
+                throw new Exception("Error initializing MongoDB", ex);
             }
         }
 
@@ -61,7 +59,7 @@ namespace PublicFunction.DataBase.Mongo
             }
             catch (Exception ex)
             {
-                throw ex;
+                throw new Exception("Insert operation failed", ex);
             }
         }
 
@@ -69,12 +67,14 @@ namespace PublicFunction.DataBase.Mongo
         {
             try
             {
-                _IMongoDatabase.GetCollection<List<Dictionary<string, object>>>(CollectionName).InsertOne(BaseCollection);
+                var collection = _IMongoDatabase.GetCollection<BsonDocument>(CollectionName);
+                var bsonList = BaseCollection.Select(dict => new BsonDocument(dict)).ToList();
+                collection.InsertMany(bsonList);
                 return true;
             }
             catch (Exception ex)
             {
-                throw ex;
+                throw new Exception("Insert operation failed", ex);
             }
         }
 
@@ -82,13 +82,65 @@ namespace PublicFunction.DataBase.Mongo
         {
             try
             {
-                _IMongoDatabase.GetCollection<Dictionary<string, object>>(CollectionName).InsertOne(BaseCollection);
+                var bsonDocument = new BsonDocument(BaseCollection);
+                _IMongoDatabase.GetCollection<BsonDocument>(CollectionName).InsertOne(bsonDocument);
                 return true;
             }
             catch (Exception ex)
             {
-                throw ex;
+                throw new Exception("Insert operation failed", ex);
+            }
+        }
+
+        public bool Update<T>(string CollectionName, FilterDefinition<T> filter, UpdateDefinition<T> update) where T : TBaseDocumentModel
+        {
+            try
+            {
+                var result = _IMongoDatabase.GetCollection<T>(CollectionName).UpdateOne(filter, update);
+                return result.ModifiedCount > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Update operation failed", ex);
+            }
+        }
+
+        public bool Delete<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel
+        {
+            try
+            {
+                var result = _IMongoDatabase.GetCollection<T>(CollectionName).DeleteOne(filter);
+                return result.DeletedCount > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Delete operation failed", ex);
+            }
+        }
+
+        public List<T> Query<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel
+        {
+            try
+            {
+                return _IMongoDatabase.GetCollection<T>(CollectionName).Find(filter).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Query operation failed", ex);
+            }
+        }
+
+        public T QueryOne<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel
+        {
+            try
+            {
+                return _IMongoDatabase.GetCollection<T>(CollectionName).Find(filter).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Query operation failed", ex);
             }
         }
     }
 }
+
--- PublicFunction/DataBase/Mongo/README.md
+++ PublicFunction/DataBase/Mongo/README.md
@@ -7,9 +7,107 @@
 
 [README.md](https://github.com/rayanabniro/PublicFunction/blob/main/PublicFunction/DataBase/README.md "README.md")
 
-# PublicFunction.Database.Mongo
 
-In this **class**, we have infrastructures that can **centrally control** the connection to the **Mongo database**
+
+# MongoDB Interaction in C#
+
+This document provides a detailed explanation of the methods defined in the `Mongo` class which interact with a MongoDB database. Below are the functions and their descriptions:
+
+## Class: `Mongo`
+
+The `Mongo` class is responsible for connecting to the MongoDB database and performing CRUD operations.
+
+### Constructor: `Mongo(IConfiguration configuration)`
+Initializes a new instance of the `Mongo` class, setting up a connection to the MongoDB database.
+
+- **Parameters**: 
+  - `IConfiguration configuration`: Configuration object to read MongoDB connection details.
+
+---
+
+## Method: `bool Insert<T>(string CollectionName, T BaseCollection) where T : TBaseDocumentModel`
+
+Inserts a single document into the specified collection.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection where the document will be inserted.
+  - `T BaseCollection`: The document to be inserted (inherits from `TBaseDocumentModel`).
+  
+- **Returns**: `true` if the insertion was successful, throws an exception otherwise.
+
+---
+
+## Method: `bool Insert(string CollectionName, List<Dictionary<string, object>> BaseCollection)`
+
+Inserts multiple documents into the specified collection.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection.
+  - `List<Dictionary<string, object>> BaseCollection`: A list of documents, where each document is represented as a dictionary.
+  
+- **Returns**: `true` if the insertion was successful, throws an exception otherwise.
+
+---
+
+## Method: `bool Insert(string CollectionName, Dictionary<string, object> BaseCollection)`
+
+Inserts a single document, represented as a dictionary, into the specified collection.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection.
+  - `Dictionary<string, object> BaseCollection`: The document to be inserted.
+  
+- **Returns**: `true` if the insertion was successful, throws an exception otherwise.
+
+---
+
+## Method: `bool Update<T>(string CollectionName, FilterDefinition<T> filter, UpdateDefinition<T> update) where T : TBaseDocumentModel`
+
+Updates a document in the specified collection based on the given filter.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection.
+  - `FilterDefinition<T> filter`: The filter to select the document to be updated.
+  - `UpdateDefinition<T> update`: The update definition to apply to the document.
+  
+- **Returns**: `true` if the update was successful, throws an exception otherwise.
+
+---
+
+## Method: `bool Delete<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel`
+
+Deletes a document from the specified collection based on the given filter.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection.
+  - `FilterDefinition<T> filter`: The filter to select the document to be deleted.
+  
+- **Returns**: `true` if the deletion was successful, throws an exception otherwise.
+
+---
+
+## Method: `List<T> Query<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel`
+
+Queries for documents that match the specified filter in the given collection.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection.
+  - `FilterDefinition<T> filter`: The filter to match documents.
+  
+- **Returns**: A list of documents matching the filter, or an empty list if no documents are found.
+
+---
+
+## Method: `T QueryOne<T>(string CollectionName, FilterDefinition<T> filter) where T : TBaseDocumentModel`
+
+Queries for a single document that matches the specified filter in the given collection.
+
+- **Parameters**:
+  - `string CollectionName`: The name of the collection.
+  - `FilterDefinition<T> filter`: The filter to match the document.
+  
+- **Returns**: The document matching the filter, or `null` if no document is found.
+
 
 ## Use in .netCore
 - in **appsettings.json** define Connection string like this
@@ -24,33 +122,3 @@ In this **class**, we have infrastructures that can **centrally control** the co
           }
         }
     ```
-- in **Program.cs**  Add This dependence
-  ```C#
-    builder.Services.AddSingleton<IConfiguration>(_ => configuration);
-  ```
-- in **Program.cs**  Add This dependence
-  ```C#
-    builder.Services.AddScoped<PublicFunction.Framework.DataBase.Mongo.IMongo, PublicFunction.Framework.DataBase.Mongo.Mongo>();
-  ```
-- Suppose you want to use functions in a class, just write the following code in its constructor function
-  ```C#
-    public class Test
-    	{
-    		private readonly PublicFunction.Framework.DataBase.Mongo.IMongo MongoService;
-    		public BeforeLoginService(PublicFunction.Framework.DataBase.Mongo.IMongo mongoService)
-    		{
-    		    MongoService = mongoService;
-    		}
-    	}
-  ```
-
-  ## Function
-    ```C#
-        //insert T Model to Mongo Collection
-        public bool Insert<T>(string CollectionName, T BaseCollection) where T : TBaseDocumentModel;
-        //insert List<Dictionary<string, object>> to Mongo Collection
-        public bool Insert(string CollectionName, List<Dictionary<string, object>> BaseCollection);
-        //insert Dictionary<string, object> to Mongo Collection
-        public bool Insert(string CollectionName, Dictionary<string, object> BaseCollection);
-   ```
-
