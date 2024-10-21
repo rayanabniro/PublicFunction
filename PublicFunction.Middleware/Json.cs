91ad90f3ac5cb77d8c54f7e918ec3600684e01fe
javaheri <asdasdasdasd@example.com>
2024-10-21 07:07:16 +0330

    Fix

---
+++ PublicFunction.Middleware/Json.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+
+namespace PublicFunction.Converter
+{
+    public class Json
+    {
+        public T Deserialize<T>(string json)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string Serialize<T>(T obj)
+        {
+            try
+            {
+                return JsonSerializer.Serialize(obj);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
